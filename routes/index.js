3a6eb27b14083b3001fb7f201c290f6bd5cdfdcf
Miguel-Pariente <miguelpl32@hotmail.com>
2021-06-13 09:05:31 +0200

    Autenticar usuarios

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const homeController = require('../controllers/homeController');
 const vacantesController = require('../controllers/vacantesController');
 const usuariosController = require('../controllers/usuariosController');
+const authController = require('../controllers/authController');
 
 module.exports = () => {
     router.get('/', homeController.mostrarTrabajos);
@@ -21,6 +22,10 @@ module.exports = () => {
     // Crear cuentas usuario
     router.get('/crear-cuenta', usuariosController.formCrearCuenta);
     router.post('/crear-cuenta', usuariosController.validarRegistro, usuariosController.crearUsuario);
+
+    // Autenticar Usuarios
+    router.get('/iniciar-sesion', usuariosController.formIniciarSesion);
+    router.post('/iniciar-sesion', authController.autenticarUsuario);
 
     return router;
 }
