5b258b110aa46d2eb9a5b9532162bdab83b13a26
Miguel-Pariente <miguelpl32@hotmail.com>
2021-06-13 09:28:35 +0200

    fix index.js

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
-const mongoose = require('mongoose');
-require('./config/db');
+const mongoose = require("mongoose");
+require("./config/db");
 
-const express = require('express');
-const exphbs = require('express-handlebars');
-const path = require('path');
-const router = require('./routes');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const expressValidator = require('express-validator');
-const flash = require('connect-flash');
-const passport = require('./config/passport');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const router = require("./routes");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const expressValidator = require("express-validator");
+const flash = require("connect-flash");
+const passport = require("./config/passport");
 
-require('dotenv').config({ path : 'variables.env'});
-
+require("dotenv").config({ path: "variables.env" });
 
 const app = express();
 
 // habilitar leer los datos del formulario
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Validacion de campos instalar esta version npm install express-validator@5.3.1
 app.use(expressValidator());
 
-//habilar handlebars como view  
-app.engine('handlebars', exphbs({ defaultLayout: 'layout', helpers: require('./helpers/handlebars')}));
+//habilar handlebars como view
+app.engine(
+    "handlebars",
+    exphbs({
+        defaultLayout: "layout",
+        helpers: require("./helpers/handlebars"),
+    })
+);
 
-app.set('view engine', 'handlebars');
+app.set("view engine", "handlebars");
 
-// static files 
-app.use(express.static(path.join(__dirname, 'public')));
+// static files
+app.use(express.static(path.join(__dirname, "public")));
 
+// Acceder  a la configuracion cookies y sesion
 app.use(cookieParser());
 
-app.use(session({
-    secret: process.env.SECRETO,
-    key: process.env.KEY,
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({mongoUrl: process.env.DATABASE})
-}));
+app.use(
+    session({
+        secret: process.env.SECRETO,
+        key: process.env.KEY,
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({ mongoUrl: process.env.DATABASE }),
+    })
+);
 
 // Inicializar passport
 app.use(passport.initialize());
@@ -55,6 +63,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/', router());
+app.use("/", router());
 
 app.listen(process.env.PUERTO);
